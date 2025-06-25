ac9c21cbb6d2077b6d86063841351d9d73c3565d
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-06-25 22:53:21 +0600

    Mobile Menu Added

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //Mobile Menu Toggle
+    $('.hamburger').click(function(){
+        $('.mobile-sidebar').addClass('active')
+        $('.overlay').addClass('active')
+    })
+})
