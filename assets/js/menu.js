14de7303a6836a67cdac16eb36d1873321905ab4
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-10 16:19:45 +0600

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const mobileSidebar = document.querySelector(".mobile-sidebar");
+  const overlay = document.querySelector(".overlay");
+  const closeButton = document.querySelectorAll(
+    ".close-button, .overlay"
+  );
 
+  if (hamburger) {
+    hamburger.addEventListener("click", function () {
+      mobileSidebar.classList.add("active");
+      overlay.classList.add("active");
+    });
+  }
+
+  closeButton.forEach(function (el) {
+    el.addEventListener("click", function () {
+      mobileSidebar.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+  });
+});
